d4bb44392c79761b684241757c7d30d7b6ae3f6b
Tim Smith <tsmith84@gmail.com>
2015-03-09 21:28:59 -0700

    Use ohai attributes to build the disable source/dest command
    Ohai already pulls this data from AWS if you use the ec2 hint file

--- recipes/ec2.rb
+++ recipes/ec2.rb
@@ -1,13 +1,10 @@
 include_recipe "awscli"
 
-instance_region = `curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | grep -Po "(us|sa|eu|ap)-(north|south)?(east|west)?-[0-9]+"`.strip
-instance_id = `curl -s http://169.254.169.254/latest/meta-data/instance-id`
-
 execute "Disable Source/Dest. check" do
   command <<-EOH
-    aws ec2 --region "#{instance_region}" \
+    aws ec2 --region "#{node['ec2']['placement_availability_zone'].chop}" \
       modify-instance-attribute \
-      --instance-id "#{instance_id}" \
+      --instance-id "#{node['ec2']['instance_id']}" \
       --no-source-dest-check
   EOH
 end
